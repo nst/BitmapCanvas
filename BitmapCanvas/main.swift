2682d6dc15de8fee1225107b4b601fbde993a9fc
Nicolas Seriot <nicolas@seriot.ch>
2016-03-05 22:37:02 +0100

    added code and files to draw thematic map of Switzerland cantons

--- BitmapCanvas/main.swift
+++ BitmapCanvas/main.swift
@@ -9,78 +9,85 @@
 import Foundation
 import AppKit
 
-func test() {
-    
-    var b = BitmapCanvas(640, 480)
+func switzerland() {
 
-    b[0,0] = NSColor.redColor()
-    b[1,0] = NSColor.greenColor()
-    b[2,0] = NSColor.blueColor()
-    
-    b.setAllowsAntialiasing(false)
-    
-    b.line(P(1,1), P(200,200))
-    
-    b.lineHorizontal(P(2,1), width: 100)
-    b.lineHorizontal(P(1,3), width: 100, color:NSColor.cyanColor())
-    b.lineHorizontal(P(1,5), width: 100)
-    
-    b.lineVertical(P(2,10), height: 100)
-    
-    b.rectangle(R(3, 3, 3, 3))
-    
-    b.rectangle(R(200, 200, 100, 50), strokeColor: NSColor.redColor())
-    
-    b.rectangle(R(300, 300, 100, 50), strokeColor: NSColor.redColor(), fillColor: Crayons["Aqua"])
-    
-    b.text("asd", P(250,250))
-    b.text("asd", P(250,250), rotationRadians:CGFloat(-M_PI/2.0))
-    b.text("asd", P(500,300), rotationDegrees:-90)
-    
-    b.line(P(400,350), P(450,450))
-    b.lineHorizontal(P(100,100), width: 50)
-    b.lineVertical(P(100,100), height: 50)
-    
-    b.setAllowsAntialiasing(true)
-    
-    CGContextAddEllipseInRect(b.cgContext, R(100, 100, 80, 50))
-    CGContextStrokePath(b.cgContext)
-    
-    NSColor.orangeColor().setFill()
-    
-    let bp = NSBezierPath()
-    bp.moveToPoint(P(70,30))
-    bp.curveToPoint(P(100,70), controlPoint1: P(68,57), controlPoint2: P(75,76))
-    bp.curveToPoint(P(157,66), controlPoint1: P(127,69), controlPoint2: P(127,97))
-    bp.closePath()
-    bp.fill()
-    bp.stroke()
-    
-    b.image(fromPath:"/usr/share/httpd/icons/sphere2.png", P(20,20))
-    
-    b.setAllowsAntialiasing(false)
-    
-    b.rectangle(R(3,3,3,3), strokeColor:NSColor.blueColor())
-    
-    b[0,0] = NSColor.cyanColor()
+    guard let resultsData = NSData(contentsOfFile: "/Users/nst/Projects/BitmapCanvas/files/results.json") else { return }
+    guard let optResults = try? NSJSONSerialization.JSONObjectWithData(resultsData, options: []) as? [String:AnyObject] else { return }
+    guard let results = optResults else { return }
 
-    b[1,1] = NSColor.redColor()
-    b[2,2] = NSColor.greenColor()
-    b[3,3] = NSColor.yellowColor()
+    guard let switzerlandData = NSData(contentsOfFile: "/Users/nst/Projects/BitmapCanvas/files/switzerland.json") else { return }
+    guard let optSwitzerland = try? NSJSONSerialization.JSONObjectWithData(switzerlandData, options: []) as? [String:AnyObject] else { return }
+    guard let switzerland = optSwitzerland else { return }
     
-    b.setAllowsAntialiasing(true)
+    let b = BitmapCanvas(365, 235, backgroundColor: NSColor.whiteColor())
+
+    b.image(fromPath: "/Users/nst/Projects/BitmapCanvas/files/switzerland.gif", P(0,0))
     
-    b[6,1] = NSColor.redColor()
-    b[7,2] = NSColor.greenColor()
-    b[8,3] = NSColor.yellowColor()
+    b.fill(P(0,0), color: NSColor.whiteColor())
+    b.fill(P(0,170), color: NSColor.whiteColor())
+    b.fill(P(300,0), color: NSColor.whiteColor())
+    b.fill(P(355,110), color: NSColor.whiteColor())
+    b.fill(P(300,190), color: NSColor.whiteColor())
+
+    let font = NSFont(name: "Monaco", size: 10)!
+    b.text("2016-02-28 \"Pas de spéculation sur les denrées alimentaires\"", P(5,220), font:font)
     
-    b.rectangle(R(8,3,3,3), strokeColor:NSColor.blueColor())
+    let values : [Double] = results.flatMap { (k,v) in v as? Double }
     
-    let path = "/tmp/test.png"
+    let positiveValues = values.filter { $0 >= 50.0 }
+    let negativeValues = values.filter { $0 < 50.0 }
+    
+    let minPositive = positiveValues.minElement() ?? 0.0
+    let maxPositive = positiveValues.maxElement() ?? 0.01
+
+    let minNegative = negativeValues.minElement() ?? 0.0
+    let maxNegative = negativeValues.maxElement() ?? 0.01
+    
+    let positiveRange = maxPositive - minPositive
+    let negativeRange = maxNegative - minNegative
+    
+    print(minPositive, maxPositive)
+    print(minNegative, maxNegative)
+    
+    for (k, cantonDict) in switzerland {
+
+        guard let labelPoint = cantonDict["label"] as? [Int] else { continue }
+
+        // fill color
+        
+        var fillColor = NSColor.lightGrayColor()
+        if let percent = results[k] as? Double {
+            if percent < 50.0 {
+                let i = ((percent - minNegative) / negativeRange) - 0.15
+                fillColor = NSColor(calibratedRed: 1.0, green: CGFloat(i), blue: CGFloat(i), alpha: 1.0)
+            } else {
+                let i = (1.0 - (percent - minPositive) / positiveRange) - 0.15
+                fillColor = NSColor(calibratedRed: CGFloat(i), green: 1.0, blue: CGFloat(i), alpha: 1.0)
+            }
+        }
+        
+        // fill cantons
+        
+        let fillPoints = cantonDict["fill"] as? [[Int]] ?? [labelPoint]
+        
+        for pts in fillPoints {
+            let px = pts[0]
+            let py = pts[1]
+            let p = P(CGFloat(px), CGFloat(py))
+            b.fill(p, color: fillColor)
+        }
+        
+        // draw labels
+        
+        let px = labelPoint[0]
+        let py = labelPoint[1]
+        let p = P(CGFloat(px), CGFloat(py))
+        b.text(k, p)
+    }
+    
+    let path = "/tmp/out.png"
     
     b.save(path)
-    
-    NSWorkspace.sharedWorkspace().openFile(path)
 }
 
 func bitmap() {
@@ -103,7 +110,10 @@ func points() {
     b[2,3] = NSColor.greenColor()
     b[3,3] = NSColor.blueColor()
     
-    b.save("/tmp/bitmap_points.png")
+    print(NSColor.blueColor())
+    print(b[3,3])
+    
+    b.save("/tmp/bitmap_points.png", open:true)
 }
 
 func lines() {
@@ -194,7 +204,7 @@ func cgContext() {
     b.save("/tmp/bitmap_cgcontext.png")
 }
 
-test()
+//switzerland()
 
 bitmap()
 points()
